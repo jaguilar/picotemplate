6fb94d55a05e0b2ef701c9ab71fb5a778bc041b2
James A <799564+jaguilar@users.noreply.github.com>
2024-03-26 01:19:26 -0400

    Clean up shared_init.c

--- src/shared_init.c
+++ src/shared_init.c
@@ -34,7 +34,6 @@ extern void start_initial_tasks();
 static TaskHandle_t g_init_task;
 
 static void init_task(void *unused) {
-  stdio_init_all();
 #ifdef RASPBERRYPI_PICO_W
   if (cyw43_arch_init()) {
     printf("Wi-Fi init failed");
@@ -45,25 +44,9 @@ static void init_task(void *unused) {
   vTaskDelete(g_init_task);
 }
 
-static void launch() {
+int main(void) {
+  stdio_init_all();
   configASSERT(xTaskCreate(init_task, "__init_task", 1024, NULL, 1,
-                           /*run on core 0*/  // 1,
                            &g_init_task) == pdPASS);
   vTaskStartScheduler();
-}
-
-int main(void) {
-#if (FREE_RTOS_KERNEL_SMP && portSUPPORT_SMP == 1) && (configNUM_CORES == 2)
-  printf("Starting on both cores:\n");
-  launch();
-#elif (RUN_FREERTOS_ON_CORE == 1)
-  printf("Starting on core 1:\n");
-  multicore_launch_core1(launch);
-  while (true)
-    ;
-#else
-  printf("Starting on core 0:\n");
-  launch();
-#endif
-  return 0;
 }
