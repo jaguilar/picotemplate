2cd434550145562c761fb79f51d2b84ea33f65c9
James A <799564+jaguilar@users.noreply.github.com>
2024-04-05 22:20:23 -0400

    Add problem 5.4!

--- src/sun/cd74hc595.cc
+++ src/sun/cd74hc595.cc
@@ -3,12 +3,15 @@
 #include "cd74hc595.pio.h"
 #include "hardware/clocks.h"
 #include "hardware/pio.h"
+#include "hardware/pio_instructions.h"
 #include "hardware/structs/clocks.h"
 
 namespace jpico {
 
 Cd74Hc595DriverPio::Cd74Hc595DriverPio(Cd74Hc595DriverPio&& o)
-    : pio_(o.pio_), state_machine_(o.state_machine_) {
+    : pio_(o.pio_),
+      state_machine_(o.state_machine_),
+      output_bits_(o.output_bits_) {
   o.pio_ = nullptr;
   o.state_machine_ = 5;  // Invalid
 }
@@ -30,20 +33,23 @@ std::optional<Cd74Hc595DriverPio> Cd74Hc595DriverPio::Create(
     panic("can't add cd74hc595 to PIO\n");
   }
   uint32_t offset = pio_add_program(pio, &cd74hc595_program);
+
   pio_sm_set_consecutive_pindirs(pio, sm, config.pin_srclk, 2, true);
   pio_sm_set_consecutive_pindirs(pio, sm, config.pin_ser, 1, true);
   pio_sm_config sm_config = cd74hc595_program_get_default_config(offset);
-  sm_config_set_clkdiv(&sm_config, static_cast<double>(clock_get_hz(clk_sys)) /
-                                       config.target_frequency);
+  sm_config_set_clkdiv(&sm_config,
+                       static_cast<float>(clock_get_hz(clk_sys) * 1.0 /
+                                          config.target_frequency));
   sm_config_set_out_pins(&sm_config, config.pin_ser, 1);
   sm_config_set_sideset_pins(&sm_config, config.pin_srclk);
 
   // We output the MSB first, so we are shifting left. This means that each
   // input must be the highest 8 bits of the 32-bit word.
-  sm_config_set_out_shift(&sm_config, false, true, 8);
+  sm_config_set_out_shift(&sm_config, false, true, config.output_bits);
   pio_sm_init(pio, sm, offset, &sm_config);
+  pio_sm_exec(pio, sm, pio_encode_set(pio_y, config.output_bits - 1));
   pio_sm_set_enabled(pio, sm, true);
-  return Cd74Hc595DriverPio(pio, sm);
+  return Cd74Hc595DriverPio(config, sm);
 }
 
 }  // namespace jpico
--- src/sun/cd74hc595.h
+++ src/sun/cd74hc595.h
@@ -12,9 +12,18 @@ class Cd74Hc595DriverPio {
  public:
   struct Config {
     PIO pio;
-    uint32_t pin_srclk = 999;  // Note: pin_rclk must be pin_srclk + 1.
-    uint32_t pin_ser = 999;
+    int pin_srclk = -1;  // Note: pin_rclk must be pin_srclk + 1.
+    int pin_ser = -1;
     uint32_t target_frequency = 1'000'000;
+
+    // Number of bits to set before ticking rclk, loading those bits into
+    // the storage register of the Cd74Hc595. Currently this driver supports
+    // at most 32 bits.
+    //
+    // This is useful if you have more than one Cd74Hc595 connected in
+    // series. The highest bit will be the one first shifted out (i.e. it
+    // will be q7 or qh on the last chip in the chain.).
+    int output_bits = 8;
   };
 
   Cd74Hc595DriverPio(const Cd74Hc595DriverPio&) = delete;
@@ -26,18 +35,21 @@ class Cd74Hc595DriverPio {
   // Constructs the driver from the Config.
   static std::optional<Cd74Hc595DriverPio> Create(const Config& config);
 
-  inline void Send(uint8_t x) {
-    pio_sm_put_blocking(pio_, state_machine_, static_cast<uint32_t>(x) << 24);
+  inline void Send(uint32_t x) {
+    pio_sm_put_blocking(pio_, state_machine_, x << (32 - output_bits_));
   }
 
   uint32_t state_machine() const { return state_machine_; }
 
  private:
-  Cd74Hc595DriverPio(PIO pio, uint32_t state_machine)
-      : pio_{pio}, state_machine_{state_machine} {}
+  Cd74Hc595DriverPio(const Config& config, uint32_t state_machine)
+      : pio_{config.pio},
+        state_machine_{state_machine},
+        output_bits_(config.output_bits) {}
 
   PIO pio_;
   uint32_t state_machine_;
+  int output_bits_;
 };
 
 }  // namespace jpico
